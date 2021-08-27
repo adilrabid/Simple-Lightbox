f75c3f22392eb3e95cfdc6819e2888b7584e7f6e
Adil Rabid <rabid1819@gmail.com>
2021-08-27 13:13:39 +0600

    upload

--- script.js
+++ script.js
@@ -1,18 +1,40 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener("DOMContentLoaded", function () {
     console.log("Welcome to simple lightbox plugin");
 
     let shwocaseItems = document.querySelectorAll(".showcase-item");
     let lightbox = document.getElementById("lightbox");
+    let lightboxWrap = document.getElementById("lightbox-wrap");
+    let lightboxContent = document.getElementById("lightbox-content");
+    let lightboxActiveContainer = document.getElementById("lightbox-active-container");
     let lightboxCloseBtn = document.getElementById("lightbox-close-btn");
 
-    shwocaseItems.forEach(showcaseItem => {
-        showcaseItem.addEventListener('click', function (e) {
-            console.log(e.target);
-            lightbox.classList.add('active');
+    shwocaseItems.forEach((showcaseItem) => {
+        showcaseItem.addEventListener("click", function (e) {
+            e.preventDefault();
+            let _source = e.target.parentElement.href;
+            let _nodeName = e.target.nodeName;
+
+            let newNode = document.createElement(_nodeName)
+            newNode.src = _source;
+            newNode.style.height = "100%";
+            newNode.style.width = "auto";
+            newNode.classList.add('lightbox-active-item');
+            appendElement(lightboxActiveContainer, newNode);
+            console.log(lightboxWrap);
+            lightbox.classList.add("lb-active");
         });
 
-        lightboxCloseBtn.addEventListener('click', function (e) {
-            lightbox.classList.remove('active');
+        lightboxCloseBtn.addEventListener("click", function (e) {
+            closeLightbox();
         });
-    })
-})
+
+    });
+
+    function appendElement(parent, child) {
+        parent.appendChild(child);
+    }
+
+    function closeLightbox() {
+        lightbox.classList.remove("lb-active");
+    }
+});
